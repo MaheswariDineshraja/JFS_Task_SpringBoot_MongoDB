78394d5ae8defe0fdf98b8100141189b2da4f97d
mageshbe2003@yahoo.co.in <mageshbe2003@yahoo.co.in>
2024-08-23 12:07:15 +0800

    first commit

---
+++ user-rest-api-app/src/main/java/org/user/app/UserRestApiAppApplication.java
@@ -0,0 +1,13 @@
+package org.user.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRestApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRestApiAppApplication.class, args);
+	}
+
+}
